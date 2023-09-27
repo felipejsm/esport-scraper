3bbc8feaccb13dc5624ca0555b32a11c620e6755
felipejsm <felipejsm41@gmail.com>
2023-09-26 21:38:26 -0300

    visiting hltv website

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,24 @@ import (
 )
 
 func main() {
+	// Instantiate default collector
 	c := colly.NewCollector()
 
+	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
 	})
 
+	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("http://go-colly.org/")
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://www.hltv.org/matches?predefinedFilter=top_tier")
 }
